9b4e56122da217d7327fff429707b788ffe02c21
Ashour Merza <Ashour.Merza@erstegroup.com>
2026-03-23 16:23:00 +0100

    Fix: base path isola-verde

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  base: '/isola-verda/', // Passe "isola-verda" an deinen Repo-Namen an!
+  base: '/isola-verde/', // Passe "isola-verde" an deinen Repo-Namen an!
   plugins: [vue()],
   resolve: {
     alias: {
